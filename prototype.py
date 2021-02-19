98a058671a0172db5301bb5021af15606350013d
pierrecasenave <45551401+pierrecasenave@users.noreply.github.com>
2021-02-19 18:23:40 -0500

    Update prototype.py

--- prototype.py
+++ prototype.py
@@ -1,20 +1,16 @@
-import sys
-import numpy as np
 import pandas as pd
-import tensorflow as tf 
-import tensorflow_hub as hub
+import numpy as np
 
-def main(user_input, org_file):
+def main(input_data):
+    import tensorflow as tf 
+    import tensorflow_hub as hub
+    import tensorflow_text
+  
     embed = hub.load('https://tfhub.dev/google/universal-sentence-encoder/3')
-    orgs_table = pd.read_csv('./' + org_file).dropna(subset=['short_description']).reset_index(drop=True)
-    embeddings = embed(orgs_table.short_description)['outputs'].numpy()
-    scores = np.dot(embeddings, embed([user_input])['outputs'].numpy()[0])
-    orgs_table.insert(len(orgs_table.columns),'score',scores)
-    orgs_table.sort_values('score', ascending=False)[:15].to_csv('top15.csv', index=False)
-    return
-
-
-if __name__ == "__main__":
-    user_input = sys.argv[1]
-    org_file = sys.argv[2]
-    main(user_input, org_file)
+    orgs_table = input_data['org_table'].dropna(subset=['short_description']).reset_index(drop=True)
+    
+    user_input = input_data['user_input'].sentence.tolist()
+    embeddings = embed(user_input)['outputs'].numpy()
+    scores = np.dot(embeddings, embed(user_input)['outputs'].numpy()[0])
+    df_final = pd.Series(scores).to_frame(name='score')
+    return df_final
